fd88a5aec53291c181c853b3f8088c9ee2c3c8eb
timoh <timoh6@gmail.com>
2011-11-25 14:05:56 +0200

    Update test data files

---
+++ tests/Data/dummyKey.php
@@ -0,0 +1,20 @@
+<?php
+return array (
+  'tcrypto_key_data' => 
+  array (
+    'keys' => 
+    array (
+      'index_xxx' => 
+      array (
+        'encryption' => 'key_xxx',
+        'authentication' => 'key_yyy',
+        'time' => 123,
+      ),
+    ),
+    'meta_data' => 
+    array (
+      'primary_index' => 'index_xxx',
+      'last_key_creation_time' => 123,
+    ),
+  ),
+);
---
+++ tests/Data/sampleOptions.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'keyfile_permissions' => 0600,
+    'bytes_in_key_name' => 3,
+    'keyfile_location' => '/path/to/Keystore/default'
+);
