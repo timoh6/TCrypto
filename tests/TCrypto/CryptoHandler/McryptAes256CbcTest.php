552962fe2f1dc2da2d32138359805473b614b395
timoh <timoh6@gmail.com>
2012-02-02 11:21:44 +0200

    Fix McryptAes256CbcTest::testEncryptWithAesVectors() to use AES-256 instead of AES-128

--- tests/TCrypto/CryptoHandler/McryptAes256CbcTest.php
+++ tests/TCrypto/CryptoHandler/McryptAes256CbcTest.php
@@ -4,10 +4,10 @@ class TCrypto_CryptoHandler_McryptAes256CbcTest extends PHPUnit_Framework_TestCa
 {
     public function testEncryptWithAesVectors()
     {
-        $key = pack("H*" , '2b7e151628aed2a6abf7158809cf4f3c');
+        $key = pack("H*" , '603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4');
         $iv = pack("H*" , '000102030405060708090A0B0C0D0E0F');
         $plaintext = pack("H*" , '6bc1bee22e409f96e93d7e117393172a');
-        $expectedCiphertext = pack("H*" , '7649abac8119b246cee98e9b12e9197d');
+        $expectedCiphertext = pack("H*" , 'f58c4c04d6e5f1ba779eabfb5f7bfbd6');
         
         $aes = new TCrypto\CryptoHandler\McryptAes256Cbc();
         $ciphertext = $aes->encrypt($plaintext, $iv, $key);
